650990a9941cfb9103b6f620dcb668b2b209ba02
magician1 <kouta-k@mbm.nifty.com>
2013-11-27 23:50:59 +0900

    振替休日まで対応

---
+++ shubun.go
@@ -0,0 +1,46 @@
+package holiday
+
+// 秋分の日リスト。予測を含む。init()で初期化
+var SHUBUN_LIST = map[Date]struct{}{}
+
+// 秋分の日判定関数
+func ShubunCheker(d Date) bool {
+	if _, ok := SHUBUN_LIST[d]; ok {
+		return true
+	}
+	return false
+}
+
+var SHUBUN_DAYS = []int{
+	23, //2000年
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	22,
+	23,
+	23,
+	23,
+	22,
+	23,
+	23,
+	23,
+	22,
+	23,
+	23,
+	23,
+	22,
+	23,
+	23,
+	23,
+	22,
+	23,
+	23, //2030年
+}
